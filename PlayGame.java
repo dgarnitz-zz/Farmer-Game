f1e4d8ad6e5c894865d454249079eba175f689df
dag8 <dag8@pc2-036-l.cs.st-andrews.ac.uk>
2018-10-14 19:15:00 +0100

    implement VT class

--- PlayGame.java
+++ PlayGame.java
@@ -7,21 +7,23 @@ public class PlayGame {
         Grid myGrid = new Grid (5,5);
 
         RadishFarmer farmerOne = new RadishFarmer(myGrid, 0, 0);
-        CornFarmer farmerTwo = new CornFarmer(myGrid, 2, 0);
+        CornFarmer farmerTwo = new CornFarmer(myGrid, 2, 1);
         HT transporterOne = new HT(myGrid, 3, 0, 10);
-        Rabbit rabbitOne = new Rabbit(myGrid, 3, 3);
-        Rabbit rabbitTwo = new Rabbit(myGrid, 4, 1);
-        Beaver beaverOne = new Beaver(myGrid, 4, 0);
+        VT transporterTwo = new VT(myGrid, 2, 3, 10);
+        /* Rabbit rabbitOne = new Rabbit(myGrid, 3, 3); */
+        Rabbit rabbitTwo = new Rabbit(myGrid, 2, 2);
+        Beaver beaverOne = new Beaver(myGrid, 2, 4);
 
         /* not sure if there is a way to do this automatically on the class */
         myGrid.registerItem(farmerOne.xCoordinate, farmerOne.yCoordinate, farmerOne);
         myGrid.registerItem(farmerTwo.xCoordinate, farmerTwo.yCoordinate, farmerTwo);
         myGrid.registerItem(transporterOne.xCoordinate, transporterOne.yCoordinate, transporterOne);
-        myGrid.registerItem(rabbitOne.xCoordinate, rabbitOne.yCoordinate, rabbitOne);
+        myGrid.registerItem(transporterTwo.xCoordinate, transporterTwo.yCoordinate, transporterTwo);
+        /* myGrid.registerItem(rabbitOne.xCoordinate, rabbitOne.yCoordinate, rabbitOne); */
         myGrid.registerItem(rabbitTwo.xCoordinate, rabbitTwo.yCoordinate, rabbitTwo);
         myGrid.registerItem(beaverOne.xCoordinate, beaverOne.yCoordinate, beaverOne);
 
         Game myGame = new Game(myGrid);
-        myGame.run(10);
+        myGame.run(12);
     }
 }
---
+++ VT.java
@@ -0,0 +1,108 @@
+public class VT extends AbstractItem {
+
+    public int capacity;
+    public String type = "VT";
+
+    public VT(Grid grid, int xCoordinate, int yCoordinate, int capacity) {
+        this.grid = grid;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString(){
+        return type;
+    }
+
+    public int farmerLocation(){
+        int gridHeight = grid.getHeight();
+        for(int i=yCoordinate-1; i>=0;  i--){
+            AbstractItem farmer = grid.getItem(xCoordinate, i);
+            if(farmer instanceof RadishFarmer || farmer instanceof CornFarmer){
+                return i;
+            }
+        }
+        for(int k = yCoordinate; k<gridHeight; k++){
+            AbstractItem farmer = grid.getItem(xCoordinate, k);
+            if(farmer instanceof RadishFarmer || farmer instanceof CornFarmer){
+                return k;
+            }
+        }
+        return gridHeight+1;
+    }
+
+    public int consumerLocation(int farmerYCoordinate){
+        int gridHeight = grid.getHeight();
+        if(farmerYCoordinate < yCoordinate){
+
+            for(int m = yCoordinate - 1; m>farmerYCoordinate; m--){
+                AbstractItem checkPath = grid.getItem(xCoordinate, m);
+                if(checkPath != null){
+                    return gridHeight+1;
+                }
+            }
+
+            for(int j = yCoordinate + 1; j<gridHeight; j++){
+                AbstractItem consumer = grid.getItem(xCoordinate, j);
+                if(consumer instanceof Rabbit || consumer instanceof Beaver){
+                    return j;
+                }
+            }
+        }
+        else{
+
+            for(int m = yCoordinate + 1; m<farmerYCoordinate; m++){
+                AbstractItem checkPath = grid.getItem(xCoordinate, m);
+                if(checkPath != null){
+                    return gridHeight+1;
+                }
+            }
+
+            for(int j = yCoordinate - 1; j>=0; j--){
+                AbstractItem consumer = grid.getItem(xCoordinate, j);
+                if(consumer instanceof Rabbit || consumer instanceof Beaver){
+                    return j;
+                }
+            }
+        }
+        return gridHeight+1;
+    }
+
+    @Override
+    public void process(TimeStep timeStep) {
+        int farmerYCoordinate = farmerLocation();
+        if(farmerYCoordinate==(grid.getHeight()+1)){
+            return;
+        }
+        int consumerYCoordinate = consumerLocation(farmerYCoordinate);
+        if(consumerYCoordinate==(grid.getHeight()+1)){
+            return;
+        }
+
+        int farmerStock = grid.getStockAt(xCoordinate, farmerYCoordinate);
+        if(farmerStock > capacity){
+            grid.reduceStockAt(xCoordinate, farmerYCoordinate, capacity);
+            grid.addToStockAt(xCoordinate, consumerYCoordinate, capacity);
+        }
+        else {
+            grid.reduceStockAt(xCoordinate, farmerYCoordinate, farmerStock);
+            grid.addToStockAt(xCoordinate, consumerYCoordinate, farmerStock);
+        }
+    }
+
+    @Override
+    protected int getStock(){
+        return grid.getStockAt(yCoordinate, xCoordinate);
+    }
+
+    @Override
+    protected void addToStock(int nutrition){
+
+    }
+
+    @Override
+    protected void reduceStock(int nutrition){
+
+    }
+}
